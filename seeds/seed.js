6704590b529476f16802f3f94ddb1929026b664a
tarekmn <nytarek@gmail.com>
2022-10-27 15:56:13 -0500

    seeding relationships

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require("../config/connection");
-const { User, Blog } = require("../models");
+const { User, Blog, Comment } = require("../models");
 
 const userData = require("./userData.json");
 const blogData = require("./blogData.json");
+const commentData = require("./commentData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -13,6 +14,7 @@ const seedDatabase = async () => {
   });
 
   await Blog.bulkCreate(blogData);
+  await Comment.bulkCreate(commentData);
 
   process.exit(0);
 };
