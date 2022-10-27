69823761d47abed4cd81ce005a82338d62baa134
tarekmn <nytarek@gmail.com>
2022-10-27 15:42:22 -0500

    stashing

--- models/index.js
+++ models/index.js
@@ -1,7 +1,36 @@
 const User = require("./User");
 const Blog = require("./Blog");
+const Comment = require("./Comment");
+
+Blog.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Blog, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: "blog_id",
+});
+
+Blog.hasMany(Comment, {
+  foreignKey: "blog_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
 
 module.exports = {
   User,
   Blog,
+  Comment,
 };
