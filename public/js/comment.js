5f1ec353987df2afd20bd51b4afb1c3cc5e1ede2
tarekmn <nytarek@gmail.com>
2022-10-28 10:06:16 -0500

    pending changes

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,9 +2,9 @@ const createComment = async (event) => {
   event.preventDefault();
   console.log("correctBTN");
 
-  const cardBody = document.querySelector("#card-body");
+  const cardBody = document.querySelector(".card-body");
   const comment = document.createElement("p");
-  comment.textContent("testingcomments");
+  comment.innerHTML("testingcomments");
 
   cardBody.append(comment);
 };
