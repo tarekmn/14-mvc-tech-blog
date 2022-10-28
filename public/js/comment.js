1070d28d9b662f8d6b08b87d4aa6eb296d58c6fd
tarekmn <nytarek@gmail.com>
2022-10-28 09:18:18 -0500

    creating comments

---
+++ public/js/comment.js
@@ -0,0 +1,12 @@
+const createComment = async (event) => {
+  event.preventDefault();
+  console.log("correctBTN");
+
+  const cardBody = document.querySelector("#card-body");
+  const comment = document.createElement("p");
+  comment.textContent("testingcomments");
+
+  cardBody.append(comment);
+};
+
+document.querySelector(".btn-comment").addEventListener("click", createComment);
